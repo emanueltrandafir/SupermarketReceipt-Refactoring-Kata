632ccea3a108706028213bb27de5ea551120cb24
emily <emily@bacheconsulting.com>
2020-04-27 09:20:56 +0200

    prettify receipt

--- java/src/main/java/dojo/supermarket/ReceiptPrinter.java
+++ java/src/main/java/dojo/supermarket/ReceiptPrinter.java
@@ -27,9 +27,16 @@ public class ReceiptPrinter {
             result.append(discountPresentation);
         }
 
-        result.append("\n");
+        horizontalLine(result);
         result.append(presentTotal(receipt));
         return result.toString();
+    }
+
+    private void horizontalLine(StringBuilder result) {
+        for (int i = 0; i < this.columns; i++) {
+            result.append("-");
+        }
+        result.append("\n");
     }
 
     private String presentReceiptItem(ReceiptItem item) {
@@ -45,7 +52,7 @@ public class ReceiptPrinter {
     }
 
     private String presentDiscount(Discount discount) {
-        String name = discount.getDescription() + "(" + discount.getProduct().getName() + ")";
+        String name = discount.getDescription() + " (" + discount.getProduct().getName() + ")";
         String value = presentPrice(discount.getDiscountAmount());
 
         return formatLineWithWhitespace(name, value);
